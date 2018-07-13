a05a24cce370ac933017a4b909bed912f667abc9
aLLeXUs <mrallexus@gmail.com>
2018-07-13 22:26:32 +0300

    Task1 done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,11 +19,11 @@
 <body>
     <nav class="navbar navbar-expand navbar-dark bg-dark mb-3">
         <a class="navbar-brand" href="#">Currency market</a>
-        {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
-            {{--<span class="navbar-toggler-icon"></span>--}}
-        {{--</button>--}}
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-        {{--<div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 @section('header')
                     @component('components.headerLink', [
@@ -39,10 +39,25 @@
                     @endcomponent
                 @show
             </ul>
-        {{--</div>--}}
+            <span class="navbar-text">
+                {{ Auth::user()->name }}
+            </span>
+            <a class="nav-item nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
     </nav>
     <div class="container">
         @yield('content')
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
 </body>
 </html>
